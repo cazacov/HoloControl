d26f60aa840739f5c0036c650ab31983662106c3
Victor Cazacov <cazacov.git@gmail.com>
2016-09-21 16:40:59 +0200

    Demo Unity app

---
+++ Unity Demo/Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SphereCommands : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    private float scale = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(transform.position, Vector3.up, 2);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(transform.position, Vector3.up, -2);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.RotateAround(transform.position, Vector3.right, 2);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.RotateAround(transform.position, Vector3.right, -2);
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            scale *= 0.99f;
+            transform.localScale = new Vector3(scale, scale,scale);
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            scale *= 1.01f;
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+}
